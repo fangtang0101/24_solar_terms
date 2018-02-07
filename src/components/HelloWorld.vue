a30cec958b30028ee53d3d0a6bf38c9fa0599a41
fangtang0101 <623003648@qq.com>
2018-02-07 21:22:49 +0800

    1.24 节气 首页

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+二十四节气
+<h4 v-for='item in list'>{{item}}</h4>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      list:['立春','雨水','惊蛰','春分','清明','谷雨','立夏','小满','芒种','夏至','小暑','大暑','立秋','处暑','白露','秋分','寒露','霜降','立冬','小雪','大雪','冬至','小寒','大寒']
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
